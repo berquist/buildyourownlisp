b319273587be84d9aebc83a9fb257fb58b0f4ee5
Eric Berquist <eric.berquist@gmail.com>
2018-08-20 16:33:32 -0400

    Chapter 7 evaluation

--- parser.c
+++ parser.c
@@ -27,6 +27,48 @@ void add_history(char* unused) { }
 #include <editline/readline.h>
 #endif
 
+/* int number_of_nodes(mpc_ast_t* t) { */
+/*     int total = 1; */
+/*     if (t->children_num >= 1) { */
+/*         for (int i = 0; i < t->children_num; i++) { */
+/*             total += number_of_nodes(t->children[i]); */
+/*         } */
+/*     } */
+/*     return total; */
+/* } */
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    /* If tagged as number return it directly */
+    /* Shouldn't this be strcmp? No, because 'expr|number|regex' is
+     * the tag for a number no matter what. */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in `x` */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaning children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     /* Create some parsers */
@@ -45,7 +87,7 @@ int main(int argc, char** argv) {
       ",
       Number, Operator, Expr, Lispy);
 
-    puts("Lispy Version 0.0.0.0.2");
+    puts("Lispy Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -54,13 +96,25 @@ int main(int argc, char** argv) {
         char* input = readline("lispy> ");
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        /* printf("No you're a %s\n", input); */
 
         /* Attempt to parse the user input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             /* On success print the AST */
-            mpc_ast_print(r.output);
+            /* mpc_ast_print(r.output); */
+            /* mpc_ast_t* a = r.output; */
+            /* printf("Tag: %s\n", a->tag); */
+            /* printf("Contents: %s\n", a->contents); */
+            /* printf("Number of children: %i\n", a->children_num); */
+            /* if (a->children_num > 0) { */
+            /*     mpc_ast_t* c0 = a->children[0]; */
+            /*     printf("First Child Tag: %s\n", c0->tag); */
+            /*     printf("First Child Contents: %s\n", c0->contents); */
+            /*     printf("First Child Number of children: %i\n", c0->children_num); */
+            /* } */
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise print the error */
